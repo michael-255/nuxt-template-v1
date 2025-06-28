5fe44fb8fab0400ad945b5de0241d628cca8cf0b
michael-255 <kodiakchaos@gmail.com>
2025-06-27 20:00:47 -0400

    initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,17 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: "2025-05-15",
+  devtools: { enabled: true },
+  ssr: false,
+  nitro: { preset: "static" },
+  modules: [
+    "@nuxt/eslint",
+    "@nuxt/fonts",
+    "@nuxt/icon",
+    "@nuxt/image",
+    "@nuxt/test-utils",
+    "@nuxt/ui",
+    "@nuxtjs/supabase",
+  ],
+  app: { baseURL: "/nuxt-template-v1/" },
+});
