04d8ae276e72887f289324d071a8038465e0cd0a
michael-255 <kodiakchaos@gmail.com>
2025-06-28 16:26:48 -0400

    linting and formatting setup

--- utils/test.ts
+++ utils/test.ts
@@ -1,7 +1,7 @@
 export const func = (name: any) => {
   if (!name) {
-    throw new Error("Name is required");
+    throw new Error('Name is required')
   }
-  console.log(`Hello, ${name}!`);
-  return `Hello, ${name}!`;
-};
+  console.log(`Hello, ${name}!`)
+  return `Hello, ${name}!`
+}
